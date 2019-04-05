f932d8bd75b7c33ca072edd954d7ba2de1cbd35d
学友 <xueyoucd@gmail.com>
2019-04-05 12:31:43 +0800

    修复useContainer签名改变后的测试

--- src/usePortal.ts
+++ src/usePortal.ts
@@ -3,13 +3,15 @@ import { useContainer, GetContainer } from "./useContainer";
 
 /**
  * 使用Portal, 弹出内容
- * @param getContainer 
+ * @param getContainer
  */
 export function usePortal(getContainer?: GetContainer): [(node: React.ReactNode) => React.ReactPortal, HTMLElement] {
-    const container = useContainer(getContainer);
+    const container = useContainer("", getContainer);
 
     function renderPortal(node: React.ReactNode) {
-        if (!container) { return null; }
+        if (!container) {
+            return null;
+        }
         return ReactDOM.createPortal(node, container);
     }
 
--- tests/useContainer.spec.ts
+++ tests/useContainer.spec.ts
@@ -1,34 +1,31 @@
 import { cleanup, renderHook } from "react-hooks-testing-library";
 import { useContainer } from "../src";
 
-describe('useContainer', () => {
+describe("useContainer", () => {
     afterEach(cleanup);
 
-    test('Get Container By Default', () => {
+    test("Get Container By Default", () => {
         const { result, unmount } = renderHook(() => useContainer());
 
         expect(result.current).toBeDefined();
-        expect(result.current.tagName).toBe('DIV');
+        expect(result.current.tagName).toBe("DIV");
         expect(result.current.parentElement).toBe(document.body);
 
         unmount();
         expect(result.current.parentElement).toBe(null);
     });
 
+    test("Get Container By Element", () => {
+        const ele = document.createElement("div");
 
-    test('Get Container By Element', () => {
-        const ele = document.createElement('div');
-
-        const { result } = renderHook(() => useContainer(ele));
+        const { result } = renderHook(() => useContainer("", ele));
         expect(result.current).toBe(ele);
     });
 
-    test('Get Container By Function', () => {
-        const ele = document.createElement('div');
+    test("Get Container By Function", () => {
+        const ele = document.createElement("div");
 
-        const { result } = renderHook(() => useContainer(() => ele));
+        const { result } = renderHook(() => useContainer("", () => ele));
         expect(result.current).toBe(ele);
     });
-
-
 });
